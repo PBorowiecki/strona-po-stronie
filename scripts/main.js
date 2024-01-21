ec0e66105dc6c827acf36ff157419dc1ad2f8086
PBorowiecki <borro2603@gmail.com>
2024-01-21 11:23:48 +0100

    Add files

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const navEl = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 56) {
+    navEl.classList.add("navbar-scrolled");
+  } else if (window.scrollY < 56) {
+    navEl.classList.remove("navbar-scrolled");
+  }
+});
+
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
