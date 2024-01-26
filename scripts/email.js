9449c0f8078e08f0910d2cc7834bd9ffb9249b5f
PBorowiecki <borro2603@gmail.com>
2024-01-26 19:45:23 +0100

    new changes

---
+++ scripts/email.js
@@ -0,0 +1,20 @@
+function sendEmail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceId = "service_d8gqu6h";
+  const templateId = "template_o0jc0z1";
+
+  emailjs
+    .send(serviceId, templateId, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Twoja wiadomość została wysłana");
+    })
+    .catch((err) => console.log(err));
+}
--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,21 @@ window.addEventListener("scroll", () => {
 //     });
 //   });
 // });
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("service_tqhglud", "template_o0jc0z1", this).then(
+        function () {
+          console.log("SUCCESS!");
+          document.getElementById("contact-form").reset();
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
