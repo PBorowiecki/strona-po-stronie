1569f2152fa4e997abd5fad5ef6ee929a1c5533f
PBorowiecki <borro2603@gmail.com>
2024-02-12 21:26:49 +0100

    add a cookie policy

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,3 +27,31 @@ window.onload = function () {
       );
     });
 };
+
+/* Shows the Cookie banner */
+function showCookieBanner() {
+  let cookieBanner = document.getElementById("cb-cookie-banner");
+  cookieBanner.style.display = "block";
+}
+
+/* Hides the Cookie banner and saves the value to localstorage */
+function hideCookieBanner() {
+  localStorage.setItem("cb_isCookieAccepted", "yes");
+  let cookieBanner = document.getElementById("cb-cookie-banner");
+  cookieBanner.style.display = "none";
+}
+
+/* Checks the localstorage and shows Cookie banner based on it. */
+function initializeCookieBanner() {
+  let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
+  if (isCookieAccepted === null) {
+    localStorage.setItem("cb_isCookieAccepted", "no");
+    showCookieBanner();
+  }
+  if (isCookieAccepted === "no") {
+    showCookieBanner();
+  }
+}
+// Assigning values to window object
+window.onload = initializeCookieBanner();
+window.cb_hideCookieBanner = hideCookieBanner;
